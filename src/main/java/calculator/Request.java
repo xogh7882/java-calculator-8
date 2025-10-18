74ae8f3bde818fafc45973e65fef64aac05f25e7
이태호 <xogh7882@naver.com>
2025-10-18 21:47:25 +0900

    feat: add separate
    - "," , ":" 을 구분자로 가지는 문자열 덧셈 기능 구현

--- src/main/java/calculator/Request.java
+++ src/main/java/calculator/Request.java
@@ -12,13 +12,18 @@ public class Request {
 
     public int[] separateNumbers() {
         ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < input.length(); i++) {
-            // input 구분자로 나누기
-        }
+        separate(numbers);
         int[] result = new int[numbers.size()];
         for (int i = 0; i < result.length; i++) {
             result[i] = numbers.get(i);
         }
         return result;
+    }
+
+    public void separate(ArrayList<Integer> numbers) {
+        String[] numbersArray = input.split(",|:");
+        for (String number : numbersArray) {
+            numbers.add(Integer.parseInt(number));
+        }
     }
 }
